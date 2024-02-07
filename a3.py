77ab96784a61f51aec217244a280c2cd2a6dc8df
JPellet123 <jwpell7802@gmail.com>
2024-02-07 14:49:02 -0400

    Commenting addBorders

--- a3.py
+++ a3.py
@@ -5,13 +5,20 @@ from PIL import Image, ImageDraw
 
 
 def addBorders(imageFileName, thickness, colour, outputFileName):
+    # grabbing the image via the filename sent in
     theImage = Image.open(imageFileName)
+    # grabbing the heigh and weight (tuple) from the image.size property
     width, height = theImage.size
 
+    # just to test that we can actually draw on image
+    # along the whole width
     for x in range(0, width):
+        # only on the first level / pixel
         y = 0
+        # set the pixel color to yellow
         theImage.putpixel((x, y), colour)
 
+    # show the image
     theImage.show()
 
 
