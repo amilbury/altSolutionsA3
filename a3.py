88f9c41c83b9bae8189709d86e6d599bf26a9693
Aaronias4 <111252509+Aaronias4@users.noreply.github.com>
2024-02-07 14:11:31 -0400

    Create a3.py

---
+++ a3.py
@@ -0,0 +1,15 @@
+# Starter code for all methods
+def addBorders(imageFileName, thickness, colour, outputFileName):
+    pass
+
+
+def addDeviders(imageFileName, rows, cols, thickness, colour, outputFileName):
+    pass
+
+
+def createImageFromBinary(sourceFileName, targetFileName):
+    pass
+
+
+def saveImageAsBinary(sourceFileName, targetFileName):
+    pass
