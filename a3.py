2ceddd648eb9292ccc72038147e423ca5cb4ab36
JPellet123 <jwpell7802@gmail.com>
2024-02-07 15:32:18 -0400

    Finished AddBorders

--- a3.py
+++ a3.py
@@ -10,16 +10,53 @@ def addBorders(imageFileName, thickness, colour, outputFileName):
     # grabbing the heigh and weight (tuple) from the image.size property
     width, height = theImage.size
 
-    # just to test that we can actually draw on image
-    # along the whole width
-    for x in range(0, width):
-        # only on the first level / pixel
-        y = 0
-        # set the pixel color to yellow
-        theImage.putpixel((x, y), colour)
+    # creating a new instance of drawer
+    drawer = ImageDraw.Draw(theImage)
+
+    # setting the bounds for the top border of the picture
+    upperLeft1 = (0, 0)
+    bottomRight1 = (width, thickness)
+
+    # defining the region with the endpoints that we defined
+    region1 = [upperLeft1, bottomRight1]  # inclusive of both endpoints
+
+    # draw the line onto the picture thus adding the border
+    drawer.rectangle(region1, colour, colour)
+
+    # setting the bounds for the left border of the picture
+    upperLeft2 = (0, 0)
+    bottomRight2 = (thickness, height)
+
+    # defining the region with the endpoints that we defined
+    region2 = [upperLeft2, bottomRight2]  # inclusive of both endpoints
+
+    # draw the line onto the picture thus adding the border
+    drawer.rectangle(region2, colour, colour)
+
+    # setting the bounds for the bottom border of the picture
+    upperLeft3 = (0, height - thickness)
+    bottomRight3 = (width, height + 5)
+
+    # defining the region with the endpoints that we defined
+    region3 = [upperLeft3, bottomRight3]  # inclusive of both endpoints
+
+    # draw the line onto the picture thus adding the border
+    drawer.rectangle(region3, colour, colour)
+
+    # setting the bounds for the right border of the picture
+    upperLeft4 = (width - thickness, 0)
+    bottomRight4 = (width, height)
+
+    # defining the region with the endpoints that we defined
+    region4 = [upperLeft4, bottomRight4]  # inclusive of both endpoints
+
+    # draw the line onto the picture thus adding the border
+    drawer.rectangle(region4, colour, colour)
 
     # show the image
     theImage.show()
+    # save the image to the file with specified file name
+    theImage.save(outputFileName)
 
 
 def addDeviders(imageFileName, rows, cols, thickness, colour, outputFileName):
